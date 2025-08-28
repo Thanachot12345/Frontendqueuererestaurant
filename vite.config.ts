067ebe7bac1bb9778bf1131539432d1c88a2f09e
Guide42-ho <mophone563@gmail.com>
2025-08-29 00:39:58 +0700

    update vit

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(),],
+  base: '/',           // เน้นย้ำ path สากลสำหรับ asset
   server: {
-     open: "/home", 
+    open: "/home",
     // port: 3000,    // เปลี่ยน port
     // host: true,    // ให้เข้าจากเครือข่ายได้
     // strictPort: false, // ถ้า port ถูกใช้อยู่ จะเลือก port ใหม่อัตโนมัติ
